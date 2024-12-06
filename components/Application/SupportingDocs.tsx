c445b018bf45449ddd447633d38f1984213d0219
Munna-Khandakar <munnashisad@gmail.com>
2024-12-07 00:27:40 +0600

    payment ui added

--- components/Application/SupportingDocs.tsx
+++ components/Application/SupportingDocs.tsx
@@ -1,10 +1,8 @@
   export const SupportingDocs = () => {
     return (
-        <div className="text-justify mt-4 border">
+        <div className="text-justify mt-4">
             <h2 className="text-lg font-semibold">Supporting documents</h2>
             <div className="">
-                <img src="https://img.yumpu.com/5304029/1/500x640/guide-to-supporting-documents.jpg"/>
-                <img src="https://img.yumpu.com/5304029/1/500x640/guide-to-supporting-documents.jpg"/>
                 <img src="https://img.yumpu.com/5304029/1/500x640/guide-to-supporting-documents.jpg"/>
             </div>
         </div>
